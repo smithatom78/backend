d5df4f2a1f54204f3d0faf9fd550752c21d5c361
Smitha Jacob <smithatom78@gmail.com>
2022-01-09 19:46:00 +0530

    ch

---
+++ app.js
@@ -0,0 +1,38 @@
+var express = require("express");
+const bodyParser = require('body-parser');
+const cors = require('cors');
+var app = express();
+app.use(bodyParser.json()); // to support JSON-encoded bodies
+
+app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
+    extended: true
+}));
+app.use(cors())
+    //var PORT = process.env.port;
+    //var PORT = 5555;
+var PORT = process.env.PORT || 3000;
+//creating instance
+app.get("/", (req, res) => {
+    console.log(req);
+    res.send("My Name is smitha jacob ,i belongs to Norka Mern batch1");
+});
+app.get("/:name", (req, res) => {
+    var data = req.params.name;
+    //console.log(req);
+    res.send("you enteerd" + data);
+});
+app.post("/signup", (req, res) => {
+    //in get value in params var Fname=req.params.name;
+    var Fname = req.body.Fname;
+    var Lname = req.body.Lname;
+    // var Email = req.body.Email1;
+    //console.log(req);
+    res.send("you enteerd" + Fname + " " + Lname);
+});
+app.listen(PORT, () => {
+
+    console.log("server ready at 3000");
+})
+
+//app.get(path,handler function)
+//
